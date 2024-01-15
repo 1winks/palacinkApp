da67a6bdf170876ba6e145d9e5cc3b7291622723
Sven Winkler <sven.winkler@fer.hr>
2024-01-15 15:20:29 +0100

    novi sustav

--- IzvorniKod/Backend/src/main/java/projektr/backend/dao/PonudaRepository.java
+++
@@ -1,9 +0,0 @@
-package projektr.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projektr.backend.domain.Ponuda;
-
-public interface PonudaRepository extends JpaRepository<Ponuda, Long> {
-    // TODO: SQL pozivi za ponuda tablicu u bazi
-
-}
--- IzvorniKod/Backend/src/main/java/projektr/backend/dto/PalacinkeDTO.java
+++
@@ -1,4 +0,0 @@
-package projektr.backend.dto;
-
-public class PalacinkeDTO {
-}
