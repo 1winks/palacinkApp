fd24eec76efccf1c5540b643cdf5af0cb132b5d8
Sven Winkler <sven.winkler@fer.hr>
2024-01-24 17:56:47 +0100

    grglow 2

--- IzvorniKod/Backend/src/main/java/hr/fer/controller/PalacinkaDodatakController.java
+++ IzvorniKod/Backend/src/main/java/hr/fer/controller/PalacinkaDodatakController.java
@@ -1,5 +1,6 @@
 package hr.fer.controller;
 
+import hr.fer.domain.Dodatak;
 import hr.fer.domain.PalacinkaDodatak;
 import hr.fer.service.PalacinkaDodatakService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ public class PalacinkaDodatakController {
     private PalacinkaDodatakService palacinkaDodatakService;
 
     @GetMapping("/palacinka/{palacinkaId}")
-    public List<PalacinkaDodatak> getDodatakForPalacinka(@PathVariable Long palacinkaId) {
+    public List<Dodatak> getDodatakForPalacinka(@PathVariable Long palacinkaId) {
         return palacinkaDodatakService.getDodatakForPalacinka(palacinkaId);
     }
 }
